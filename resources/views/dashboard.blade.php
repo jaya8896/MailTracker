ccd439460da079edfab8808fb110aacd98888db9
jaya8896 <jaya8896@gmail.com>
2017-08-31 18:46:35 +0530

    cleaned dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 
   <body>
   <!-- container section start -->
-  <stion id="container" class="">
+  <section id="container" class="">
      
       
       @include('layouts.top');
@@ -63,59 +63,8 @@
 				</div><!--/.col-->
 				
 			</div><!--/.row-->
-		
-			
-           <div class="row">
-		    <div class="col-lg-9 col-md-12">
-					
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							<h2><i class="fa fa-map-marker red"></i><strong>Countries</strong></h2>
-							<div class="panel-actions">
-								<a href="index.html#" class="btn-setting"><i class="fa fa-rotate-right"></i></a>
-								<a href="index.html#" class="btn-minimize"><i class="fa fa-chevron-up"></i></a>
-								<a href="index.html#" class="btn-close"><i class="fa fa-times"></i></a>
-							</div>	
-						</div>
-						<div class="panel-body-map">
-							<div id="map" style="height:380px;"></div>	
-						</div>
-	
-					</div>
-				</div>
-              <div class="col-md-3">
-              <!-- List starts -->
-				<ul class="today-datas">
-                <!-- List #1 -->
-				<li>
-                  <!-- Graph -->
-                  <div><span id="todayspark1" class="spark"></span></div>
-                  <!-- Text -->
-                  <div class="datas-text">11,500 visitors/day</div>
-                </li>
-                <li>
-                  <div><span id="todayspark2" class="spark"></span></div>
-                  <div class="datas-text">15,000 Pageviews</div>
-                </li>
-                <li>
-                  <div><span id="todayspark3" class="spark"></span></div>
-                  <div class="datas-text">30.55% Bounce Rate</div>
-                </li>
-                <li>
-                  <div><span id="todayspark4" class="spark"></span></div>
-                  <div class="datas-text">$16,00 Revenue/Day</div>
-                </li> 
-                <li>
-                  <div><span id="todayspark5" class="spark"></span></div>
-                  <div class="datas-text">12,000000 visitors every Month</div>
-                </li>                                                                                                              
-              </ul>
-              </div>
-              
-			 
-           </div>  
-            
-		  
+
+
 		  <!-- Today status end -->
 
                     
@@ -130,6 +79,7 @@
 
           </section>
       </section>
+  </section>
       <!--main content end-->
   <!-- container section start -->
 
